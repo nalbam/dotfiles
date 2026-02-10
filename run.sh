276a80aa782198075487a37f30d04f2e40956a35
nalbam <me@nalbam.com>
2026-02-11 01:39:22 +0900

    refactor: Remove tmux help script download for cleanup

--- run.sh
+++ run.sh
@@ -772,7 +772,6 @@ _download .profile profile
 _download .aliases aliases
 _download .vimrc vimrc
 _download .tmux.conf tmux.conf
-_download .tmux-help.sh tmux-help.sh
 _download .zshrc zshrc
 _download .zprofile $OS_NAME/zprofile.$OS_ARCH.sh
 _ok "Shell configuration files applied"
